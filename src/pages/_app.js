fd6d24853ab3593c1c2a951edfbf210dffe6ca56
ibqn <evgen.ibqn@gmail.com>
2021-09-30 22:15:27 +0200

    use roboto font from node modules. fix meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,11 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from 'theme'
 import GlobalStyle from 'components/global-style'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
